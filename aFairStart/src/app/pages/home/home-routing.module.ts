07610a6c0d32e1e64093b4b336c741f4726cdb85
Felix Bick <welcome@felix-media.com>
2022-08-15 10:22:45 +0200

    added Player
    *Player
    -added Player Component
    -added Smal Player in Home
    
    *CreateBooking view
    - fixed spacing and more

--- aFairStart/src/app/pages/home/home-routing.module.ts
+++ aFairStart/src/app/pages/home/home-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePage } from './home.page';
+import { PlayerComponent } from './player/player.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage
+  },
+  {
+    path: 'player',
+    component: PlayerComponent
   }
 ];
 
